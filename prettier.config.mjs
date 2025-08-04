f1b513613c6bf42a24dc62a80a01105695d328fc
Vladyslav Ivashko <vladyslavivashko@MacBook-Air.local>
2025-08-04 22:57:42 +0300

    feat: initial app

---
+++ prettier.config.mjs
@@ -0,0 +1,13 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'auto',
+  plugins: [],
+};

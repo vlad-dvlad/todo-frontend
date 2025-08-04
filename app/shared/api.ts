6844679d26b2646b579d63a414d81134d86ea293
Vladyslav Ivashko <vladyslavivashko@MacBook-Air.local>
2025-08-05 00:07:30 +0300

    feat: basic axios config

---
+++ app/shared/api.ts
@@ -0,0 +1,38 @@
+import axios, {  AxiosResponse } from 'axios'
+
+let controller: AbortController | null = null
+
+const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+// Перехоплення запитів
+axiosInstance.interceptors.request.use(config => {
+  // Якщо є попередній запит — скасовуємо
+  if (controller) {
+    controller.abort()
+  }
+
+  controller = new AbortController()
+  config.signal = controller.signal
+
+  return config
+})
+
+// Перехоплення відповідей
+axiosInstance.interceptors.response.use(
+  (response: AxiosResponse) => {
+    controller = null
+    return response
+  },
+  error => {
+    controller = null
+    return Promise.reject(error)
+  }
+)
+
+export default axiosInstance
